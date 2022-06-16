fbafe59759edcdbde6b19d8fff5a70ee2b8da7d2
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-06-17 00:48:32 +0900

    feat: handle pagination in `get-my-external-pr-list`

--- get-my-external-pr-list/index.mjs
+++ get-my-external-pr-list/index.mjs
@@ -1,16 +1,35 @@
-import "zx/globals"
+import "zx/globals";
 
-const ENDPOINT = "https://api.github.com/search/issues"
-const username = "KoichiKiyokawa"
-const ignoreOwners = ["KoichiKiyokawa"]
+/** @typedef {{ repository_url: string, pull_request: { html_url: string } }} IssueItem */
 
-const q = `type:pr author:${username}`
-const prList = await fetch(ENDPOINT + "?q=" + q).then((r) => r.json())
-const externalPRs = prList.items.filter(
-  (pr) =>
-    !ignoreOwners.some((owner) =>
-      pr.repository_url.startsWith(`https://api.github.com/repos/${owner}`)
-    )
-)
+const ENDPOINT = "https://api.github.com/search/issues";
+const username = "KoichiKiyokawa";
+const ignoreOwners = ["KoichiKiyokawa"];
 
-console.log(externalPRs.map((pr) => pr.pull_request.html_url))
+const q = `type:pr author:${username}`;
+let page = 1;
+/** @type {IssueItem[]} */
+const prList = [];
+while (1) {
+  const res = await fetchPrList(page++);
+  if (res.message?.startsWith("API rate limit")) throw Error("API rate limit");
+  if (res.items.length === 0) break;
+  prList.push(...res.items);
+}
+
+const externalPRs = prList.filter(
+  (pr) => !ignoreOwners.some((owner) => pr.repository_url.startsWith(`https://api.github.com/repos/${owner}`))
+);
+
+console.log(externalPRs.map((pr) => pr.pull_request.html_url));
+
+/**
+ * @param {number} page
+ * @returns {Promise<{ message?: string, items: IssueItem[] }>}
+ */
+function fetchPrList(page) {
+  return fetch(
+    ENDPOINT + "?" + new URLSearchParams({ q, status: "all", owned: "false", per_page: "100", page: String(page) }),
+    { headers: { accept: "application/vnd.github.v3+json" } }
+  ).then((r) => r.json());
+}
